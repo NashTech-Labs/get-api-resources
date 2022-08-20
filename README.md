777dc6b26bd3ef4ad3b333a986104c8505424617
yamikarajput546 <yamikarajput.233@gmail.com>
2022-08-20 18:18:56 +0530

    updated

---
+++ README.md
@@ -0,0 +1,30 @@
+# get-api-resources
+we can use the client module to interact with the kubernetes resources. 
+
+But In Python, we instantiate AdmissionregistrationV1Api class from client module:
+
+`client_api = client.AdmissionregistrationV1Api()`         
+
+Here I've created the client with it's respective class AdmissionregistrationV1Api
+and storing in a var named as client_api. so furture we can use it.
+
+`KubeConfig:` to pass the on local cluster e.g minikube we use bellowcommand: 
+
+`config. load_kube_config()`
+
+#### Authenticating to the Kubernetes API server
+
+But what if you want to list all the automated ingress of a GKE Cluster, you must need to authenticate the configuration
+
+`configuration.api_key = {"authorization": "Bearer" + bearer_token}` 
+
+I've used Bearer Token which enable requests to authenticate using an access key.
+
+#### get the list of all api resources:
+
+call the function get_pods(cluster_details)
+
+And run following command:
+
+`python3 get-api-resources.py`
+
---
+++ get-api-resources.py
@@ -0,0 +1,41 @@
+from kubernetes import client, utils 
+import kubernetes.client
+from kubernetes.client.rest import ApiException
+from kubernetes import client, config
+
+def __get_kubernetes_client(bearer_token,api_server_endpoint):
+    try:
+        configuration = kubernetes.client.Configuration()
+        configuration.host = api_server_endpoint
+        configuration.verify_ssl = False
+        configuration.api_key = {"authorization": "Bearer " + bearer_token}
+        client.Configuration.set_default(configuration)
+        with kubernetes.client.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+            api_instance1 = kubernetes.client.AdmissionregistrationV1Api(api_client)
+        return api_instance1
+
+    except ApiException as e:
+        print("Error getting kubernetes client:\n{}".format(e.body))
+        print("TYPE :{}".format(type(e)))
+        return None
+
+
+def get_api_resources(cluster_details):
+
+    client_api= __get_kubernetes_client(
+    bearer_token=cluster_details["bearer_token"],
+    api_server_endpoint=cluster_details["api_server_endpoint"],
+        )
+    
+    ret =client_api.get_api_resources()
+    print("get the api resources: {}".format(ret))
+           
+ 
+
+if __name__ == '__main__':
+    cluster_details={
+        "bearer_token":"GKE-Bearer-Token",
+        "api_server_endpoint":"Ip-k8s-control-plane"
+    }
+    get_api_resources(cluster_details)
